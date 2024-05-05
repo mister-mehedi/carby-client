392495a43fecb0535c7acc3ad31b25277f132702
mister_mehedi <mehedi.hasan.the.official@gmail.com>
2024-05-05 19:37:51 +0600

    home-nav,banner,about,footer part demo done

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet } from 'react-router-dom';
+import Footer from '../Pages/Shared/Footer/Footer';
+import Navbar from '../Pages/Shared/Navbar/Navbar';
+
+const Root = () => {
+  return (
+    <div>
+      <div className='max-w-7xl mx-auto'>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+
+    </div>
+  );
+};
+
+export default Root;
